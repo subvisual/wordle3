3966f737777f6f69a05d6596a62c74cd60792895
Rui Sousa <contact@ruisousa.me>
2024-10-28 13:59:40 +0000

    format and fix

--- src/Wordle.sol
+++ src/Wordle.sol
@@ -2,17 +2,11 @@
 pragma solidity ^0.8.28;
 
 contract Wordle {
+    // setup the hidden word here
+    string private HIDDEN_WORD = "birdy";
 
-  // setup the hidden word here
-  string private HIDDEN_WORD = "birdy";
-  
-  // string comparator
-  function compareWords(
-    string calldata wordA,
-    string calldata wordB
-  ) public returns (bool) {
-    return keccak256(abi.encodePacked(wordA)) == keccak256(abi.encodePacked(wordB));
-  }
+    // string comparator
+    function compareWords(string calldata wordA, string calldata wordB) public returns (bool) {
+        return keccak256(abi.encodePacked(wordA)) == keccak256(abi.encodePacked(wordB));
+    }
 }
-
-
--- test/Wordle.t.sol
+++ test/Wordle.t.sol
@@ -4,18 +4,16 @@ import {Test} from "forge-std/test.sol";
 import {Wordle} from "../src/Wordle.sol";
 
 contract WordleTest is Test {
-  Wordle wordle;
+    Wordle wordle;
 
-  function setUp() public {
-    wordle = new Wordle();
-  }
+    function setUp() public {
+        wordle = new Wordle();
+    }
 
-  function test_compareWords() public {
-    bool fail = wordle.compareWords("hello", "world");
-    bool success = wordle.compareWords("hello", "hello");
-    assertEq(fail, false);
-    assertEq(success, true);
-  }
-
+    function test_compareWords() public {
+        bool fail = wordle.compareWords("hello", "world");
+        bool success = wordle.compareWords("hello", "hello");
+        assertEq(fail, false);
+        assertEq(success, true);
+    }
 }
-
