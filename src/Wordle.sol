950492486b5984120f59da42ddb31208c554945e
Rui Sousa <contact@ruisousa.me>
2024-10-28 13:53:54 +0000

    add string compartor

---
+++ src/Wordle.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Wordle {
+
+  // setup the hidden word here
+  string private HIDDEN_WORD = "birdy";
+  
+  // string comparator
+  function compareWords(
+    string calldata wordA,
+    string calldata wordB
+  ) public returns (bool) {
+    return keccak256(abi.encodePacked(wordA)) == keccak256(abi.encodePacked(wordB));
+  }
+}
+
+
---
+++ test/Wordle.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/test.sol";
+import {Wordle} from "../src/Wordle.sol";
+
+contract WordleTest is Test {
+  Wordle wordle;
+
+  function setUp() public {
+    wordle = new Wordle();
+  }
+
+  function test_compareWords() public {
+    bool fail = wordle.compareWords("hello", "world");
+    bool success = wordle.compareWords("hello", "hello");
+    assertEq(fail, false);
+    assertEq(success, true);
+  }
+
+}
+
