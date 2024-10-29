bb9683d73de5ddcad621eeaa7421425792289e4a
Rui Sousa <contact@ruisousa.me>
2024-10-29 09:00:21 +0000

    rename: main.sol -> wordle.sol

--- src/Main.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-contract Wordle {
-    // setup the hidden word here
-    string private HIDDEN_WORD = "birdy";
-}
--- src/Wordle.sol
+++ src/Wordle.sol
@@ -4,29 +4,4 @@ pragma solidity ^0.8.28;
 contract Wordle {
     // setup the hidden word here
     string private HIDDEN_WORD = "birdy";
-
-    // certify that string is ASCII
-    function isASCII(string memory word) public pure returns (bool) {
-        bytes memory wordBytes = bytes(word);
-
-        for (uint256 i = 0; i < wordBytes.length; i++) {
-            if (uint8(wordBytes[i]) > 0x7F) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    // string comparator
-    function compareWords(string calldata target, string calldata guess) public returns (bool) {
-        if (!isASCII(target) || !isASCII(guess)) {
-            revert("Non-ASCII word detected.");
-        }
-
-        if (bytes(target).length != bytes(guess).length) {
-            revert("Target and Guess are not the same size.");
-        }
-
-        return false;
-    }
 }
